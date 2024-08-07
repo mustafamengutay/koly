6b6597057d35be1fcf3ba5bec04e125bd93512fa
Mustafa <mustafamengutayy@gmail.com>
2024-08-07 19:13:21 +0300

    feat: add custom http error types

---
+++ server/types/errors.ts
@@ -0,0 +1,18 @@
+import { ValidationError } from 'express-validator';
+
+export class HttpError extends Error {
+  public statusCode: number;
+  constructor(statusCode: number, message: string) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+export class HttpBodyValidationError extends HttpError {
+  public errors: ValidationError[];
+  constructor(statusCode: number, errors: ValidationError[]) {
+    super(statusCode, 'Field Validation Error');
+    this.statusCode = statusCode;
+    this.errors = errors;
+  }
+}
