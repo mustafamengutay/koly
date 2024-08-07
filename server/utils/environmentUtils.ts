dba55e4c9259822066e8be7ae111dd1fe77c08e7
Mustafa <mustafamengutayy@gmail.com>
2024-08-07 16:36:29 +0300

    feat: add environment checker

---
+++ server/utils/environmentUtils.ts
@@ -0,0 +1,24 @@
+/**
+ *
+ * @param variables An array includes environment variable names.
+ * @description It is a function which is used to check environment variables.
+ * It throws an error if an empty array is passed, or there is a missing environment
+ * variable.
+ */
+export const checkEnvironmentVariables = (variables: string[]) => {
+  if (variables.length === 0) {
+    throw new Error(
+      'The array is empty. Please add some environment variables into the array.'
+    );
+  }
+
+  for (const variable of variables) {
+    if (!(variable in process.env)) {
+      throw new Error(
+        `${variable} is missing in .env file. Check ${variable} and its value.`
+      );
+    }
+  }
+
+  console.log('OK');
+};
