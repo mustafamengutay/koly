9ab522856f2c0500f72d0500715010d5f575a84a
Mustafa Mengutay <48598974+mustafamengutay@users.noreply.github.com>
2024-12-09 22:41:25 +0200

    feat: add form validation

--- client/src/components/LoginForm/LoginForm.tsx
+++ client/src/components/LoginForm/LoginForm.tsx
@@ -4,47 +4,86 @@ import { Field } from '../ui/field';
 import { PasswordInput } from '../ui/password-input';
 import { Button } from '../ui/button';
 import { Link } from 'react-router';
+import { useForm } from 'react-hook-form';
+
+interface LoginFormValues {
+  email: string;
+  password: string;
+}
 
 export default function LoginForm() {
-  return (
-    <Card.Root as='form' borderRadius='lg' width='24rem'>
-      <Card.Body gap='4' alignItems='center'>
-        <Logo />
-        <Card.Description>Log in to continue to koly</Card.Description>
-        <Field label='Email' required>
-          <Input
-            padding='6'
-            placeholder='example@email.com'
-            borderRadius='lg'
-          />
-        </Field>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormValues>();
 
-        <Field label='Password' required>
-          <PasswordInput padding='6' placeholder='password' borderRadius='lg' />
-        </Field>
-      </Card.Body>
-      <Card.Footer gap='4' justifyContent='center' flexDirection='column'>
-        <Button
-          variant='solid'
-          padding='6'
-          size='md'
-          width='full'
-          borderRadius='lg'
-        >
-          Login
-        </Button>
-        <Link to='/signup' style={{ width: '100%' }}>
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+
+  return (
+    <form onSubmit={onSubmit}>
+      <Card.Root borderRadius='lg' width='24rem'>
+        <Card.Body gap='4' alignItems='center'>
+          <Logo />
+          <Card.Description>Log in to continue to koly</Card.Description>
+          <Field
+            label='Email'
+            invalid={!!errors.email}
+            errorText={errors.email?.message}
+          >
+            <Input
+              padding='6'
+              placeholder='example@email.com'
+              borderRadius='lg'
+              {...register('email', {
+                required: 'Email is required',
+                pattern: {
+                  value: /\S+@\S+\.\S+/,
+                  message: 'Entered value does not match email format',
+                },
+              })}
+            />
+          </Field>
+
+          <Field
+            label='Password'
+            invalid={!!errors.password}
+            errorText={errors.password?.message}
+          >
+            <PasswordInput
+              padding='6'
+              placeholder='password'
+              borderRadius='lg'
+              {...register('password', { required: 'Password is required' })}
+            />
+          </Field>
+        </Card.Body>
+        <Card.Footer gap='4' justifyContent='center' flexDirection='column'>
           <Button
-            variant='outline'
+            type='submit'
+            variant='solid'
             padding='6'
             size='md'
             width='full'
             borderRadius='lg'
           >
-            Create an account
+            Login
           </Button>
-        </Link>
-      </Card.Footer>
-    </Card.Root>
+          <Link to='/signup' style={{ width: '100%' }}>
+            <Button
+              variant='outline'
+              padding='6'
+              size='md'
+              width='full'
+              borderRadius='lg'
+            >
+              Create an account
+            </Button>
+          </Link>
+        </Card.Footer>
+      </Card.Root>
+    </form>
   );
 }
