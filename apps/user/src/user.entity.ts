30f66bdf17e54e3ade2a0b4f8cd7a62dbacea10a
Mustafa Mengutay <48598974+mustafamengutay@users.noreply.github.com>
2025-02-21 19:54:55 +0200

    feat: add user entity

---
+++ apps/user/src/user.entity.ts
@@ -0,0 +1,31 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  surname: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
