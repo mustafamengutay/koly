cc5a23345741447859dc94a93484d668b6bbe697
Mustafa Mengutay <48598974+mustafamengutay@users.noreply.github.com>
2025-02-21 19:54:29 +0200

    feat: init user microservice

---
+++ apps/user/src/user.controller.spec.ts
@@ -0,0 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+
+describe('UserController', () => {
+  let userController: UserController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [UserController],
+      providers: [UserService],
+    }).compile();
+
+    userController = app.get<UserController>(UserController);
+  });
+
+  describe('root', () => {
+    it('', () => {});
+  });
+});
