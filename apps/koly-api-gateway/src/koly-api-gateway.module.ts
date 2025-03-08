d55e9e35625d68ef8d6e8f887658d6790cb404b9
Mustafa Mengutay <48598974+mustafamengutay@users.noreply.github.com>
2025-03-08 23:22:53 +0200

    feat: config env variable

--- apps/koly-api-gateway/src/koly-api-gateway.module.ts
+++ apps/koly-api-gateway/src/koly-api-gateway.module.ts
@@ -3,9 +3,17 @@ import { KolyApiGatewayController } from './koly-api-gateway.controller';
 import { KolyApiGatewayService } from './koly-api-gateway.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: 'apps/koly-api-gateway/.env',
+    }),
+    UserModule,
+    AuthModule,
+  ],
   controllers: [KolyApiGatewayController],
   providers: [KolyApiGatewayService],
 })
